62c493f1051cfe1581d9faffd066862bcc236333
Toke Friis <tokefriis@gmail.com>
2019-01-24 18:55:01 +0100

    Listevisning og defekte links

--- webapp/app.py
+++ webapp/app.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from flask import Flask
+import os
+
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
 
+@app.route('/billeder')
+def billeder():
+    file_names = []
+    with os.scandir('/home/toke/Pictures') as it:
+        for entry in it:
+            if not entry.name.startswith('.') and entry.is_file():
+                file_names.append(entry.name)
+    return render_template('billeder.html', html_file_names=file_names)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
