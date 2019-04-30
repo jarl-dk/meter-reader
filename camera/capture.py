1ec7565d02882743e72604add9a38d16f4842ae3
Jarl Friis <jarl@softace.dk>
2019-04-30 23:34:13 +0200

    Added rotation, resolution and zoom settings

--- camera/capture.py
+++ camera/capture.py
@@ -5,4 +5,8 @@ from datetime import datetime
 
 camera = PiCamera()
 
+camera.sharpness = 100
+camera.rotation = 180
+camera.zoom = (0.0, 0.0, 1.0, 1.0)
+camera.resolution = (1280, 720)
 camera.capture('/home/pi/Pictures/billede_{0}.jpg'.format(datetime.utcnow().replace(microsecond=0).isoformat()))
